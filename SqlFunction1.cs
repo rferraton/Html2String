b5eb08c15964ead185d0b203075a7ecedecd7879
Romain Ferraton <rferraton@users.noreply.github.com>
2023-08-02 08:29:55 +0200

    integrate webutility HTMLDecode

--- SqlFunction1.cs
+++ SqlFunction1.cs
@@ -5,32 +5,29 @@ using System.Collections.Generic;
 using Microsoft.SqlServer.Server;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Net;
+
 public partial class UserDefinedFunctions
 {
-	[Microsoft.SqlServer.Server.SqlFunction]
-	public static SqlString CleanHTML(SqlString s)
+	[SqlFunction(IsDeterministic = true, IsPrecise = true, DataAccess = DataAccessKind.None)]
+	public static SqlString CleanHTML(string s) => pCleanHTML(s);
+
+	private static string pCleanHTML(string s)
 	{
-		if (s.IsNull) return String.Empty;
-		string s1 = s.ToString().Trim();
-		if (s1.Length == 0) return String.Empty;
-		StringBuilder tmpS = new StringBuilder(s1.Length);
-		//striping out the "control characters"
-		if (!Char.IsControl(s1[0])) tmpS.Append(s1[0]);
-		for (int i = 1; i <= s1.Length - 1; i++)
-		{
-			if (Char.IsControl(s1[i]))
-			{
-				if (s1[i - 1] != ' ') tmpS.Append(' ');
-			}
-			else
-			{
-				tmpS.Append(s1[i]);
-			}
-		}
-		string result = tmpS.ToString();
-		//finding the HTML tags and replacing them with an empty string
-		string pattern = @"<[^>]*?>|<[^>]*>";
-		Regex rgx = new Regex(pattern);
-		return rgx.Replace(result, String.Empty);
+		
+		if (string.IsNullOrEmpty(s))
+			return null;
+
+
+		var html = s;
+		
+		html = Regex.Replace(html, "<br/>", Environment.NewLine, RegexOptions.IgnoreCase);
+		html = Regex.Replace(html, "<br />", Environment.NewLine, RegexOptions.IgnoreCase);
+		html = Regex.Replace(html, "<br>", Environment.NewLine, RegexOptions.IgnoreCase);	
+		html = Regex.Replace(html, "&nbsp;", Environment.NewLine, RegexOptions.IgnoreCase);
+		html = WebUtility.HtmlDecode(html);
+		html = Regex.Replace(html, "<.*?>", string.Empty);
+
+		return html;
 	}
 }
